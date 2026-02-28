83ab7e24ec0267dc6ad90837f059874b1e2ff9c7
emilfriis <erafh@me.com>
2026-02-28 15:26:52 +0100

    package file

--- packages.py
+++ packages.py
@@ -10,15 +10,3 @@ import pandas as pd                 # for data manipulation and analysis
 import numpy as np                  # for numerical operations
 import matplotlib.pyplot as plt     # for plotting
 import yfinance as yf               # for downloading financial data
-
-# -------------------
-# Estimation methods
-# -------------------
-import statsmodels.tsa.ar_model import AutoReg # autoregressive models
-
-# -------------------
-# GitHub, file handling etc.
-# -------------------
-import sys                              # provides access to Pythons search path
-import os                               # gives Python access to file system 
-sys.path.append(os.path.abspath(".."))  # search for modules in parent directory
