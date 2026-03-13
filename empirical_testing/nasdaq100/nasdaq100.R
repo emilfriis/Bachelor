57a7db4338bba7ea0a4fc6de31603688679ecf5e
emilfriis <erafh@me.com>
2026-03-13 11:51:25 +0100

    nasdaq100

---
+++ empirical_testing/nasdaq100/nasdaq100.R
@@ -0,0 +1,30 @@
+library(exuber)
+
+# 1. Load series
+dat <- read.csv('empirical_testing/nasdaq100/data/nasdaq100.csv')
+y   <- ts(dat$Close)     # change after $ to the column name
+T   <- length(y)
+
+# 2. Test statistics (ADF, SADF, GSADF)
+res  <- radf(y, lag = 0)
+
+# 3. Critical values (Monte Carlo)
+cv   <- radf_mc_cv(n = T, nrep = 2000, seed = 123)
+
+options(digits = 10)
+print(res)
+print(cv)
+
+# BSADF critical values (90%, 95%, 99%) and test statistic
+cv$bsadf_cv # cv
+res$bsadf   # test statistic
+
+# download BSADF data
+
+# BSADF test statistic over time
+bsadf_stat <- data.frame(bsadf = as.numeric(res$bsadf))
+write.csv(bsadf_stat, "empirical_testing/nasdaq100/results/nasdaq100_bsadf_stat.csv", row.names = FALSE)
+
+# BSADF critical values (90, 95, 99)
+bsadf_cv <- as.data.frame(cv$bsadf_cv)        # keep all significance-level columns
+write.csv(bsadf_cv, "empirical_testing/nasdaq100/results/nasdaq100_bsadf_cv.csv", row.names = FALSE)
